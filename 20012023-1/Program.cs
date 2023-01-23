e6228189248c20a885618ed615b7a51ff0bacc78
Natalya <pauls88@yandex.ru>
2023-01-23 10:56:12 +0500

    AddFile20012023-1

---
+++ 20012023-1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом");
+// (НЕ использовать число как строку, то есть сравнения типа number[0] == number[4] делать НЕЛЬЗЯ. Используем операторы % и /).
+Console.Write("Введите число, содержащее более 4 цифр: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int rebmun = 0;
+int n = number;
+
+while (number !=0)
+        {   
+            
+            int reb = number % 10; 
+            rebmun = rebmun * 10 + reb;
+            number=number/10;
+            
+        }
+if (rebmun==n)
+
+    Console.Write($"Число {n} является палиндромом.");
+    
+else
+
+    Console.Write($"Число {n} не является палиндромом.");
+   
